2cae4ae590a4e30577e347cc5ec03eb79d7fb21b
prasadrptedla@gmail.com <prasadrptedla@gmail.com>
2018-08-09 14:55:44 +0530

    changes done for first class

--- src/First.java
+++ src/First.java
@@ -1,7 +1,19 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 
 public class First {
 	  
-	
+	public static void main(String args[]) throws UnknownHostException
+	{
+		InetAddress myIP=InetAddress.getLocalHost();
+		 
+	      /* public String getHostAddress(): Returns the IP 
+	       * address string in textual presentation.
+	       */
+	      System.out.println("My IP Address is:");
+	      System.out.println(myIP.getHostAddress());
+	}
 	
 	
 }
